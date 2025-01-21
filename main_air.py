40e295b6d2f841d5f0c5bbbedeffe81148ebf6fb
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-01-21 17:05:33 +0200

    Add files via upload

---
+++ main_air.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from subprocess import call
+from threading import Thread
+
+import fly_by_ip_air
+
+Thread(target=fly_by_ip_air.openSerial).start()
+
+call(['./mediamtx'])
+
+
