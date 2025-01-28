645ba611d02a792a6470195a251ad31b61a19c34
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-01-28 09:42:29 +0200

    Add files via upload

---
+++ gnd/installer_gnd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y upgrade
+
+wget https://github.com/EugeneTsymbaliuk/optical-drone/blob/main/fly_by_ip_gnd.py
+wget https://github.com/EugeneTsymbaliuk/optical-drone/blob/main/main_gnd.py
+
+touch ~/starter.sh
+echo '#!/bin/bash' >> ~/starter.sh
+echo  >> ~/starter.sh
+echo 'python ~/main_gnd.py' >> ~/starter.sh
+chmod 755 ~/starter.sh
+
+# Change window manager (mutter to openbox-lxde)
+sudo sed -i "s/mutter/openbox-lxde/g" /etc/xdg/lxsession/LXDE-pi/desktop.conf
+cp -rf /etc/xdg/openbox/ ~/.config/
+
+# Autostart
+mkdir -p ~/.config/lxsession/LXDE-pi/
+cp /etc/xdg/lxsession/LXDE-pi/* ~/.config/lxsession/LXDE-pi/
+echo "/home/`whoami`/starter.sh" >> ~/.config/lxsession/LXDE-pi/autostart
+
+# Screensaver
+sed -i /xscreensaver/d ~/.config/lxsession/LXDE-pi/autostart
+echo 'xset s noblank' >> ~/.config/lxsession/LXDE-pi/autostart
+echo 'xset -dpms' >> ~/.config/lxsession/LXDE-pi/autostart
+echo 'xset -s off' >> ~/.config/lxsession/LXDE-pi/autostart
+echo 'chromium-browser --start-fullscreen http://192.168.10.1:8889/cam' >> ~/.config/lxsession/LXDE-pi/autostart
---
+++ gnd/main_gnd.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+import fly_by_ip_gnd
+
+fly_by_ip_gnd.openSerial()
