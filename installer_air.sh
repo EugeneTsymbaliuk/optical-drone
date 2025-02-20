68a8ed775c008b561760c7d6f63716956b3569e6
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-02-20 12:04:37 +0200

    Update installer_air.sh

--- installer_air.sh
+++ installer_air.sh
@@ -9,7 +9,7 @@ echo  >> ~/starter.sh
 echo 'python ~/optical-drone/main_air.py' >> ~/starter.sh
 chmod 755 ~/starter.sh
 
-echo "/home/`whoami`/optical-drone/mediamtx >> ~/optical-drone/main_air.py
+echo 'call(['/home/admin/optical-drone/mediamtx'])' >> ~/optical-drone/main_air.py
 
 # Change window manager (mutter to openbox-lxde)
 sudo sed -i "s/mutter/openbox-lxde/g" /etc/xdg/lxsession/LXDE-pi/desktop.conf
