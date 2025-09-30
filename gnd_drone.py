05f35cc2d4defd423d792ce19414938eb6dbceea
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-09-30 14:39:44 +0300

    Update gnd_drone.py

--- gnd_drone.py
+++ gnd_drone.py
@@ -45,7 +45,8 @@ def openSerial():
         while True:
             data, addr = sock.recvfrom(1024)
             chans = pickle.loads(data)
-            print(chans)
+            #print(chans)
+            #print(pwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[2]), pwmCalc(chans[3]))
             #rcOverrides(pwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[2]), pwmCalc(chans[3]))
     
     return bytes(chans)
