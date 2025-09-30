9b86b472c6425d6bbd2af9721369743187b62e68
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-09-30 14:12:05 +0300

    Update gnd_drone.py

--- gnd_drone.py
+++ gnd_drone.py
@@ -31,6 +31,13 @@ def sinfo():
         if l.startswith('Serial'):
             skey = l[-18:].strip()
 
+def pwmCalc(crsf_value):
+    pwm = 1500 + (0.625 * (crsf_value - 992))
+    return int(pwm)
+
+def rcOverrides(roll, pitch, thr, yaw):
+    vehicle.channels.overrides = {'1': roll, '2': pitch, '3': thr, '4': yaw}
+
 def crc8_dvb_s2(crc, a) -> int:
   crc = crc ^ a
   for ii in range(8):
@@ -89,10 +96,11 @@ def openSerial():
     with serial.Serial(args.port0, args.baud, timeout=2) as ser:
         if skey == skey:
             while True:
-                data, addr = sock.recvfrom(1024)
-                chans = pickle.loads(data)
-                ser.write(channelsCrsfToChannelsPacket(chans))
-
+                #data, addr = sock.recvfrom(1024)
+                #chans = pickle.loads(data)
+                #ser.write(channelsCrsfToChannelsPacket(chans))
+                rcOverrides(pwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[2]), pwmCalc(chans[3]))
+    
     return bytes(chans)
 
 if __name__ == "__main__":
