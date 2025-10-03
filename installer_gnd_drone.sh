0b0af12ca1834233e74204a4c40fae310c87effb
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-10-03 14:24:23 +0300

    Update installer_gnd_drone.sh

--- installer_gnd_drone.sh
+++ installer_gnd_drone.sh
@@ -6,28 +6,10 @@ sudo apt -y upgrade
 pip3 install future
 pip3 install dronekit
 
-touch ~/starter.sh
-echo '#!/bin/bash' >> ~/starter.sh
-echo  >> ~/starter.sh
-echo 'python ~/optical-drone/main_gnd_drone.py' >> ~/starter.sh
-chmod 755 ~/starter.sh
-
-echo "call(['/home/`whoami`/mediamtx'])" >> ~/optical-drone/main_gnd_drone.py
-
-# Change window manager (mutter to openbox-lxde)
-sudo sed -i "s/mutter/openbox-lxde/g" /etc/xdg/lxsession/LXDE-pi/desktop.conf
-cp -rf /etc/xdg/openbox/ ~/.config/
-
-# Autostart
-mkdir -p ~/.config/lxsession/LXDE-pi/
-cp /etc/xdg/lxsession/LXDE-pi/* ~/.config/lxsession/LXDE-pi/
-echo "/home/`whoami`/starter.sh" >> ~/.config/lxsession/LXDE-pi/autostart
-
-# Screensaver
-sed -i /xscreensaver/d ~/.config/lxsession/LXDE-pi/autostart
-echo 'xset s noblank' >> ~/.config/lxsession/LXDE-pi/autostart
-echo 'xset -dpms' >> ~/.config/lxsession/LXDE-pi/autostart
-echo 'xset -s off' >> ~/.config/lxsession/LXDE-pi/autostart
+cd ~/
+wget https://github.com/bluenviron/mediamtx/releases/download/v1.11.1/mediamtx_v1.11.1_linux_armv6.tar.gz
+tar -xvzf mediamtx_v1.11.1_linux_armv6.tar.gz
+chmod +x mediamtx
 
 rm ~/optical-drone/*gnd.*
 rm ~/optical-drone/*air*
