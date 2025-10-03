1344c5f387ef5cfcbe5b36bfd8d37a186d0dadd7
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-10-03 15:04:54 +0300

    Update main_gnd_drone.py

--- main_gnd_drone.py
+++ main_gnd_drone.py
@@ -1,10 +1,7 @@
 #! /usr/bin/env python
 
 from threading import Thread
-from subprocess import call
 import gnd_drone
-from time import sleep
 
-sleep(20)
 Thread(target=gnd_drone.openSerial).start()
 
