e822f8caafa44d2f0f4801231d8deaf6a52b7c98
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-10-03 15:10:56 +0300

    Update main_air.py

--- main_air.py
+++ main_air.py
@@ -1,11 +1,7 @@
 #! /usr/bin/env python
 
-from subprocess import call
 from threading import Thread
-
 import fly_by_ip_air
-from time import sleep
 
-sleep(10)
 Thread(target=fly_by_ip_air.openSerial).start()
 
