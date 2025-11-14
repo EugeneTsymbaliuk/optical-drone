54f35eabb9b83c328131fb2919cdf99df1f39676
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-11-14 19:59:29 +0200

    Update installer_gnd.sh

--- installer_gnd.sh
+++ installer_gnd.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt -y upgrade
-
-pip3 install future
-pip3 install dronekit
-
-touch ~/starter.sh
-echo '#!/bin/bash' >> ~/starter.sh
-echo  >> ~/starter.sh
-echo 'python ~/optical-drone/main_gnd.py' >> ~/starter.sh
-chmod 755 ~/starter.sh
-
-# Change window manager (mutter to openbox-lxde)
-sudo sed -i "s/mutter/openbox-lxde/g" /etc/xdg/lxsession/LXDE-pi/desktop.conf
-cp -rf /etc/xdg/openbox/ ~/.config/
-
-# Autostart
-mkdir -p ~/.config/lxsession/LXDE-pi/
-cp /etc/xdg/lxsession/LXDE-pi/* ~/.config/lxsession/LXDE-pi/
-echo "/home/`whoami`/starter.sh" >> ~/.config/lxsession/LXDE-pi/autostart
-
-# Screensaver
-sed -i /xscreensaver/d ~/.config/lxsession/LXDE-pi/autostart
-echo 'xset s noblank' >> ~/.config/lxsession/LXDE-pi/autostart
-echo 'xset -dpms' >> ~/.config/lxsession/LXDE-pi/autostart
-echo 'xset -s off' >> ~/.config/lxsession/LXDE-pi/autostart
-echo 'chromium-browser --start-fullscreen http://192.168.10.1:8889/cam' >> ~/.config/lxsession/LXDE-pi/autostart
-
 rm ~/optical-drone/*air*
 rm ~/optical-drone/*gnd_drone*
