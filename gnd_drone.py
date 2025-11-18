d333c1b59bc5cf9123c37a0d2214e3304d8211da
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-11-18 15:46:40 +0200

    Update gnd_drone.py

--- gnd_drone.py
+++ gnd_drone.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 """
-Check CPU Serial Number. 
+Check CPU Serial Number.
 cat /proc/cpuinfo
 """
 
 import socket
 import pickle
+import struct
 from time import time, sleep
 from threading import Thread
 from dronekit import connect, VehicleMode
@@ -14,15 +15,20 @@ from dronekit import connect, VehicleMode
 chans = []
 
 # UDP IP address and port to listen on
-UDP_IP = "0.0.0.0" 
+UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
 
-sskey = '0000000006f12a37'
+sskey = '00000000ca127702'
 
 # Create the connection to drone
 print('Connecting to FC')
 #vehicle = connect('tcp:192.168.1.145:5762', rate=40)
+#while not True:
+#    try:
 vehicle = connect("/dev/ttyAMA0", baud=57600, wait_ready=True,  timeout=100, rate=40)
+#    except dronekit.APIException:
+#        print("FC Unavailable")
+
 print('Connected to FC')
 
 def sinfo():
@@ -33,11 +39,12 @@ def sinfo():
             skey = l[-18:].strip()
 
 def pwmCalc(crsf_value):
-    pwm = 1500 + (0.625 * (crsf_value - 992))
+#    pwm = 1500 + (0.625 * (crsf_value - 992))
+    pwm = crsf_value * 0.61 + 894
     return int(pwm)
 
-def rcOverrides(roll, pitch, thr, yaw):
-    vehicle.channels.overrides = {'1': roll, '2': pitch, '3': thr, '4': yaw}
+def rcOverrides(roll, pitch, thr=1500, yaw=1500, arm=2000):
+    vehicle.channels.overrides = {'1': roll, '2': pitch, '3': thr, '4': yaw, '6': arm}
 
 def openSerial():
     global chans
@@ -49,10 +56,21 @@ def openSerial():
             data, addr = sock.recvfrom(1024)
             chans = pickle.loads(data)
             if chans[0] > 1:
-                #print(chans)
-                #print(pwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[2]), pwmCalc(chans[3]))
-                rcOverrides(pwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[2]), pwmCalc(chans[3]))
-    
+            #print(chans)
+               # print(chans[0], chans[1])
+            #rcOverrides(chans[0], chans[1], chans[2], chans[3], chans[5])
+            #print(pwmCalc(chans[0]), pwmCalc(chans[1]))
+            #rcOverrides(pwmCalc(chans[0]), pwmCalc(chans[1]))
+#                try:
+#                    if pwmCalc(chans[0]) and pwmCalc(chans[1]) < 2100:
+                print(pwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[5]))
+                rcOverrides(pwmCalc(chans[0]), pwmCalc(chans[1]), arm=pwmCalc(chans[5]))
+#                except struct.error:
+#                    pass
+#                except NameError:
+#                    pass
+            sleep(0.01)
+
     return bytes(chans)
 
 if __name__ == "__main__":
