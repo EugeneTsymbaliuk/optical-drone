3f438424aa478939b1ae849aa69609e4e507b5e1
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-11-26 12:35:57 +0200

    Update gnd_drone.py

--- gnd_drone.py
+++ gnd_drone.py
@@ -37,8 +37,8 @@ def pwmCalc(crsf_value):
     pwm = crsf_value * 0.61 + 894
     return int(pwm)
 
-def rcOverrides(roll, pitch, thr=1500, yaw=1500, arm=2000):
-    vehicle.channels.overrides = {'1': roll, '2': pitch, '3': thr, '4': yaw, '6': arm}
+def rcOverrides(ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9, ch10):
+    vehicle.channels.overrides = {'1': ch1, '2': ch2, '3': ch3, '4': ch4, '5': ch5, '6': ch6, '7': ch7, '8': ch8, '9': ch9, '10': ch10}
 
 def openSerial():
     global chans
@@ -53,8 +53,8 @@ def openSerial():
                 chans = pickle.loads(data)
                 if chans[0] > 1:
                     #print(chans[0], chans[1])
-                    #print(pwmCalc(chans[0]), pwmCalc(chans[1]))
-                    rcOverrides(pwmCalc(chans[0]), pwmCalc(chans[1]), arm=pwmCalc(chans[5]))
+                    #printpwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[2]), pwmCalc(chans[3]), pwmCalc(chans[4]), pwmCalc(chans[5]), pwmCalc(chans[6]), pwmCalc(chans[7]), pwmCalc(chans[8]), pwmCalc(chans[9])
+                    rcOverrides(pwmCalc(chans[0]), pwmCalc(chans[1]), pwmCalc(chans[2]), pwmCalc(chans[3]), pwmCalc(chans[4]), pwmCalc(chans[5]), pwmCalc(chans[6]), pwmCalc(chans[7]), pwmCalc(chans[8]), pwmCalc(chans[9]))
                     sleep(0.01)
             except TimeoutError:
                 #print("STOP!")
