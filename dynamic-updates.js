24ef9cd4d74d9e03e8ddcf414d6e08f0d635fb64
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-12-09 16:22:47 +0200

    Update dynamic-updates.js

--- dynamic-updates.js
+++ dynamic-updates.js
@@ -17,24 +17,16 @@ function updateLiveData() {
 
     // Sats number
     fetchDada(file_sats, 'sats')
-    //document.getElementById('sats').textContent =
-    //    Math.floor(Math.random() * 1000);
-
+    
     // Volts
     fetchDada(file_volts, 'volts')
-    //document.getElementById('volts').textContent =
-    //    Math.floor(Math.random() * 1000);
-
+   
     // Amps
     fetchDada(file_amps, 'amps')
-    // document.getElementById('amps').textContent =
-    //     Math.floor(Math.random() * 1000);
-
+   
     // Heading
     fetchDada(file_heading, 'head')
-    //document.getElementById('head').textContent =
-    //    Math.floor(Math.random() * 1000);
-
+ 
     // Update timestamp
     updateTimestamp();
 }
