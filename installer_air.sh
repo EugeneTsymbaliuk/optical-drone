967fa3c8dd3852c7258c022e5ce01f08f5134600
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-12-10 14:32:51 +0200

    Update installer_air.sh

--- installer_air.sh
+++ installer_air.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt -y upgrade
-
 cd ~/
 wget https://github.com/bluenviron/mediamtx/releases/download/v1.11.1/mediamtx_v1.11.1_linux_armv6.tar.gz
 tar -xvzf mediamtx_v1.11.1_linux_armv6.tar.gz
