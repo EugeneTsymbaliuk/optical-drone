4234650fa3c265706cf2d161de7f819c14a37ed1
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-12-10 14:00:19 +0200

    Update installer_gnd_drone.sh

--- installer_gnd_drone.sh
+++ installer_gnd_drone.sh
@@ -6,6 +6,12 @@ sudo apt -y upgrade
 pip3 install future
 pip3 install dronekit
 
+mv ~/optical-drone/index.html ~/index.html 
+mv ~/optical-drone/dynamic-updates.js ~/dynamic-updates.js
+mkdir files/
+mv ~/optical-drone/files/* ~/files/
+mv ~/optical-drone/files/* ~/files/
+
 cd ~/
 wget https://github.com/bluenviron/mediamtx/releases/download/v1.11.1/mediamtx_v1.11.1_linux_armv6.tar.gz
 tar -xvzf mediamtx_v1.11.1_linux_armv6.tar.gz
@@ -13,6 +19,3 @@ chmod +x mediamtx
 
 rm ~/optical-drone/*gnd.*
 rm ~/optical-drone/*air*
-
-mv ~/optical-drone/index.html ~/index.html 
-mv ~/optical-drone/dynamic-updates.js ~/dynamic-updates.js
