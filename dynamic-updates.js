b125ac8cf457c0b76da3fa4c381093d9e911c6ff
Eugene Tsymbaliuk <31383416+EugeneTsymbaliuk@users.noreply.github.com>
2025-12-11 10:58:44 +0200

    Add files via upload

--- dynamic-updates.js
+++ dynamic-updates.js
@@ -2,6 +2,10 @@ let file_volts = 'files/volts.txt';
 let file_amps = 'files/amps.txt';
 let file_sats = 'files/sats.txt';
 let file_heading = 'files/heading.txt';
+let file_arm = 'files/arm.txt';
+let file_gspeed = 'files/gspeed.txt';
+let file_lat = 'files/lat.txt';
+let file_lon = 'files/lon.txt';
 
 function fetchDada(file, item) {
     fetch(file)
@@ -26,6 +30,18 @@ function updateLiveData() {
    
     // Heading
     fetchDada(file_heading, 'head')
+
+    // Heading
+    fetchDada(file_arm, 'arm')
+
+    // Heading
+    fetchDada(file_gspeed, 'gspeed')
+
+    // Heading
+    fetchDada(file_lat, 'lat')
+
+    // Heading
+    fetchDada(file_lon, 'lon')
  
     // Update timestamp
     updateTimestamp();
